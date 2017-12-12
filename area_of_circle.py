9969a5732ffd93608446ad05d5e31d599222bc5a
chrynx <ralphmadriaga@gmail.com>
2017-12-12 20:17:58 +0000

    translated area_of_circle to python3

--- area_of_circle.py
+++ area_of_circle.py
@@ -1,9 +1,9 @@
-print 'Hello and welcome to this area calculator for a circle'
-radius = int(raw_input('Please enter the radius of the circle -> ' ))
+print('Hello and welcome to this area calculator for a circle')
+radius = float(input('Please enter the radius of the circle -> ' ))
 import math
 
 def area_of_circle(radius):
  area = 2 * math.pi * radius
- print 'The area of the cirle is ' + str(area)
+ print('The area of the cirle is ' + str(area))
 
 area_of_circle(radius)
